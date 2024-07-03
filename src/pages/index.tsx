2993eee93995d018ecada815ef4d47a489cb7e3a
Daniel Whiffing <danielwhiffing@gmail.com>
2024-07-03 19:49:49 -0400

    Landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,158 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
+    <main>
+      <NavBar />
+
+      <Hero />
+
+      <div className="flex flex-col items-center gap-y-2 px-10 py-20 text-center">
+        <p className="font-garamond text-2xl">
+          Subscribe to the Starving Artist Myth Newsletter and receive a free
+          excerpt.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        <p className="text-md font-garamond">
+          Plus, get a discount code worth 10% off purchase of the book from the
+          Dundurn Press Online Store
+        </p>
+        <p className="font-garamond text-xs">(conditions apply)</p>
+
+        <Button className="my-3" href="/" label="Subscribe" />
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <Footer />
     </main>
   );
 }
+
+function NavBar() {
+  return (
+    <nav className="hidden md:block">
+      <div className="fixed inset-x-0 top-0 flex justify-end gap-8 p-5 font-din text-xl text-black">
+        {/* <Link href="/">HOME</Link>
+        <Link href="/">THE BOOK</Link>
+        <Link href="/">THE AUTHOR</Link>
+        <Link href="/">CREATIVE CAREERS</Link>
+        <Link href="/">NEWSLETTER</Link>
+        <Link href="/">CONTACT</Link> */}
+      </div>
+    </nav>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="flex h-[136px] flex-col gap-8 p-5">
+      <div className="flex items-center justify-center gap-6 md:gap-12">
+        <Link href="/">
+          <Image
+            width={40}
+            height={40}
+            alt="facebook"
+            src="/social/facebook.png"
+          />
+        </Link>
+        <Link href="/">
+          <Image
+            width={40}
+            height={40}
+            alt="instagram"
+            src="/social/instagram.png"
+          />
+        </Link>
+        <Link href="/">
+          <Image
+            width={40}
+            height={40}
+            alt="linkedin"
+            src="/social/linkedin.png"
+          />
+        </Link>
+        <Link href="/">
+          <Image
+            width={40}
+            height={40}
+            alt="tiktok"
+            src="/social/tik-tok.png"
+          />
+        </Link>
+        <Link href="/">
+          <Image width={40} height={40} alt="x" src="/social/twitter.png" />
+        </Link>
+      </div>
+      <span className="text-center font-garamond">
+        © {new Date().getFullYear()} Starving Artist Myth Enterprises Inc.
+      </span>
+    </footer>
+  );
+}
+
+function Hero() {
+  return (
+    <div className="bg-hero flex min-w-[375px] flex-col items-center justify-center gap-8 px-6 py-10 font-interstate text-xl text-black md:flex-row md:px-12 md:py-20 md:text-2xl">
+      <div className="relative aspect-square w-[300px] md:w-[500px]">
+        <Image
+          alt="book"
+          sizes="100vw"
+          objectFit="contain"
+          fill
+          src="/starving-artist-render.png"
+        />
+      </div>
+
+      <div className="flex flex-col justify-center gap-y-8">
+        <div className="flex max-w-xl flex-col gap-2">
+          <h2 className="font-interstate-black text-3xl uppercase tracking-[.2em] xl:text-5xl">
+            Mark J. Jones
+          </h2>
+          <h3 className="">
+            Dean, Faculty of Animation, Arts, and Design at Sheridan College
+          </h3>
+        </div>
+
+        <div className="flex max-w-[350px] items-center justify-center bg-yellow px-3 py-4">
+          <div className="font-league text-6xl uppercase">
+            <span className="block text-3xl">The</span>
+            <span className="-mt-1 block">Starving Artist</span>
+            <span className="relative -mb-1 -mt-3 block font-cake normal-case text-red">
+              Myth
+            </span>
+          </div>
+        </div>
+
+        <div className="flex max-w-xl flex-col gap-4">
+          <p className="">
+            Bust the Stereotype and Find Success in Creative Careers
+          </p>
+          <p className="font-interstate-black text-2xl uppercase tracking-widest">
+            Available August 20, 2024
+          </p>
+        </div>
+
+        <Button href="/" label="Pre-order here" />
+
+        <Image
+          className=""
+          alt="book"
+          src="/dundurn.png"
+          height={150}
+          width={150}
+        />
+      </div>
+    </div>
+  );
+}
+
+const Button = (props: { className?: string; href: string; label: string }) => (
+  <Link href={props.href}>
+    <button
+      className={`bg-yellow px-6 py-3 text-black transition-colors hover:bg-red hover:text-white ${props.className ?? ""}`}
+    >
+      <span className="mt-[3px] block font-din text-2xl uppercase leading-none">
+        {props.label}
+      </span>
+    </button>
+  </Link>
+);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,39 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        yellow: '#fadd27',
+        red: '#c52a1d',
+        orange: '#d05d29',
+        // Yellow-orange tip of pencil: #e9b227
+        black: '#050001',
+      },
+      fontFamily: {
+        avenir: ['AvenirLTStd'],
+        cake: ['CreamCake'],
+        din: ['DINCondensed'],
+        gilroy: ['Gilroy'],
+        helvetica: ['HelveticaNeue'],
+        league: ['LeagueGothic'],
+        garamond: ['Garamond'],
+        interstate: ['Interstate'],
+        'interstate-bold': ['InterstateBold'],
+        'interstate-black': ['InterstateBlack'],
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
